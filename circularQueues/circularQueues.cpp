3d83001beecee188f0572298d5504099f335aea1
finanovita <finanovita479@gmail.com>
2024-05-30 10:33:23 +0700

    membuat class queues

---
+++ circularQueues/circularQueues.cpp
@@ -0,0 +1,17 @@
+// circularQueues.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class Queues {
+    int FRONT, REAR, max = 5;
+    int queue_array[5];
+
+};
+
+int main()
+{
+    
+}
+
